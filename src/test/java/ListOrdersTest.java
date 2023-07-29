072c804fa1b7f4a1d04b09cb582dc58a4488af0d
Nina Pelikh <pelikhnina@yandex.ru>
2023-07-30 00:44:49 +0100

    fix

--- src/test/java/ListOrdersTest.java
+++ src/test/java/ListOrdersTest.java
@@ -1,13 +1,8 @@
 import io.restassured.http.ContentType;
 import io.restassured.response.ValidatableResponse;
-import org.example.courier.CourierClient;
 import org.example.order.OrderClient;
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-
-import java.util.HashSet;
-import java.util.Set;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.empty;
@@ -17,17 +12,12 @@ public class ListOrdersTest {
 
     private static final String BASE_URI = "https://qa-scooter.praktikum-services.ru";
     private static final String API_V1 = "/api/v1";
-    private final CourierClient courierClient = new CourierClient();
-    private final OrderClient orderClient = new OrderClient();
-    private Set<String> createdCourierLogins = new HashSet<>();
-    private Set<Integer> createdOrderTracks = new HashSet<>();
+        private final OrderClient orderClient = new OrderClient();
 
     @Before
     public void setup() {
         given().baseUri(BASE_URI).basePath(API_V1).contentType(ContentType.JSON).when().get("/ping").then().statusCode(200);
     }
-
-
 
     @Test
     public void listOrdersTest() {
